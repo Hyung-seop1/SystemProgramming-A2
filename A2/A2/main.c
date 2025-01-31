7ca08ae143652ca85e38b9fa2943c70d46f5d912
Hyungseop Lee <hlee8291@conestogac.on.ca>
2025-01-31 13:49:16 -0500

    Implement -h command in main

--- A2/A2/main.c
+++ A2/A2/main.c
@@ -2,5 +2,13 @@
 
 
 int main(int argc, char* argv[]) {
+
+	// If Command is "-h" display usage statement and exit the program
+	if (argc == 2 && strcmp(argv[1], "-h") == 0) {
+		printf("Usage: encodeInput [-iinputFile] [-ooutputFile] [-srec | -asm]\n");
+		return 0;
+	}
+
 	return 0;
 }
+
