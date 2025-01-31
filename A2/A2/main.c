14a853e9ea0b5a08d1dbab92089210c4609eb3ea
Hyungseop Lee <hlee8291@conestogac.on.ca>
2025-01-31 16:11:38 -0500

    parsing the argument from main.c

--- A2/A2/main.c
+++ A2/A2/main.c
@@ -2,13 +2,38 @@
 
 
 int main(int argc, char* argv[]) {
+    char* inputFilename = NULL;
+    char* outputFilename = NULL;
+    FILE* inputFile = NULL;
+    FILE* outputFile = NULL;
+    int srec_format = 0; // Flag for S-Record format
 
-	// If Command is "-h" display usage statement and exit the program
-	if (argc == 2 && strcmp(argv[1], "-h") == 0) {
-		printf("Usage: encodeInput [-iinputFile] [-ooutputFile] [-srec | -asm]\n");
-		return 0;
-	}
+    // Parse command-line arguments
+    for (int i = 1; i < argc; i++) {
+        if (strncmp(argv[i], "-h", 2) == 0) {
+            // Printing Uusage statement
+            printf("Usage: encodeInput -iInputFile -oOutputFile -srec|-asm -h\n");
+            return 0;
+        }
+        else if (strncmp(argv[i], "-i", 2) == 0) {
+            inputFilename = argv[i] + 2;  // Skip the "-i" part and get the filename
+            printf("%s", inputFilename);
+        }
+        else if (strncmp(argv[i], "-o", 2) == 0) {
+            outputFilename = argv[i] + 2; // Skip the "-o" part and get the filename
+            printf("%s", outputFilename);
+        }
+        else if (strncmp(argv[i], "-srec", 5) == 0) {
+            srec_format = 1;
+        }
+        else if (strncmp(argv[i], "-asm", 4) == 0) {
+            // Default format, no action needed
+        }
+        else {
+            printf("Usage: encodeInput -iInputFile -oOutputFile -srec|-asm -h\n");
+            return 1;
+        }
+    }
 
-	return 0;
+    return 0;
 }
-
