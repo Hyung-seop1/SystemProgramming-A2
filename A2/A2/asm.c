61998aea7244d60f24102357c1631697d14d2b1a
Elibe Deborah <elibechilo@gmail.com>
2025-01-31 21:35:23 -0500

    Enabled functionality in the ASM.c file

--- A2/A2/asm.c
+++ A2/A2/asm.c
@@ -1,1 +1,30 @@
 #include "encoder.h"
+
+void convertToASM(char* filename, FILE* outputFile)
+{
+	FILE* fp =stdin
+		if (filename)
+		{
+			fp = fopen(filename, "rb");
+
+			if (filename == NULL)
+			{
+				printf("error opening file for reading");
+				return -1;
+			}
+
+	    }
+	char buffer[1024] = { 0 };
+	int bytes_read = 0;
+
+	while ((bytes_read = fread(buffer, sizeof(buffer), fp)) > 0)
+	{
+		//process data by chunks
+	}
+
+	if (fp!= stdin)
+	{
+		fclose(fp);
+	}
+	
+}
