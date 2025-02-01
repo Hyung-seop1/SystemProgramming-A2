c5173430bbd04d0b9608b242630e41b653924aa1
Hyungseop Lee <hlee8291@conestogac.on.ca>
2025-01-31 20:13:30 -0500

    modify main for parsing

--- A2/A2/main.c
+++ A2/A2/main.c
@@ -1,30 +1,27 @@
 #include "encoder.h"
 
-
 int main(int argc, char* argv[]) {
     char* inputFilename = NULL;
     char* outputFilename = NULL;
-    FILE* inputFile = NULL;
-    FILE* outputFile = NULL;
     int srec_format = 0; // Flag for S-Record format
 
     // Parse command-line arguments
     for (int i = 1; i < argc; i++) {
         if (strncmp(argv[i], "-h", 2) == 0) {
-            // Printing Uusage statement
+            // Print usage statement
             printf("Usage: encodeInput -iInputFile -oOutputFile -srec|-asm -h\n");
             return 0;
         }
         else if (strncmp(argv[i], "-i", 2) == 0) {
-            inputFilename = argv[i] + 2;  // Skip the "-i" part and get the filename
-            printf("%s", inputFilename);
+            // Extract input filename
+            inputFilename = argv[i] + 2;  // Skip the "-i" part
+            srecFunction(inputFilename);  // Check and process the input file
         }
         else if (strncmp(argv[i], "-o", 2) == 0) {
             outputFilename = argv[i] + 2; // Skip the "-o" part and get the filename
-            printf("%s", outputFilename);
         }
         else if (strncmp(argv[i], "-srec", 5) == 0) {
-            srec_format = 1;
+            srec_format = 1;  // Set SREC format flag
         }
         else if (strncmp(argv[i], "-asm", 4) == 0) {
             // Default format, no action needed
